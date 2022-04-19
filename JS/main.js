76ff5e809dc827cf63500a9b6354648898e282b0
IKHARO Paul <paulevansikharo@gmail.com>
2022-04-19 11:36:53 -0700

    Adjusted background image

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const hamburg = document.querySelector(".hamburger")
+const men = document.querySelector(".menu")
+
+hamburg.addEventListener('click', () => {
+    hamburg.classList.toggle('active')
+    men.classList.toggle('active')
+})
