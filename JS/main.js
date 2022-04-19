baa5ef13234ce694aaeb6aa5ee3ebc535eb3a24b
IKHARO Paul <paulevansikharo@gmail.com>
2022-04-19 16:25:38 -0700

    startted with nav bar

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
-//const hamburg = document.querySelector(".hamburger")
-//const men = document.querySelector(".menu")
-//
-//hamburg.addEventListener('click', () => {
-//    hamburg.classList.toggle('active')
-//    men.classList.toggle('active')
-//})
+const hamburg = document.querySelector(".hamburger")
+const men = document.querySelector(".menu")
+
+hamburg.addEventListener('click', () => {
+    hamburg.classList.toggle('active')
+    men.classList.toggle('active')
+})
