88de996ee109ea86658569898933e565eb17bb5f
IKHARO Paul <paulevansikharo@gmail.com>
2022-04-22 18:03:43 -0700

    changed images of choice

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,16 @@
 const hamburg = document.querySelector(".hamburger")
 const men = document.querySelector(".menu")
+const listItem = document.querySelectorAll(".menu-button")
 
 hamburg.addEventListener('click', () => {
     hamburg.classList.toggle('active')
     men.classList.toggle('active')
+})
+
+
+listItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburg.classList.remove('active')
+        men.classList.remove('active')
+    })
 })
